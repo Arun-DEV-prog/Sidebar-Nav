85e1c32a8772e623347ef85a6b9b10e6ff801c6f
Arun_DEV <arunkumarroy.cse2021@gmail.com>
2025-01-04 23:40:01 +0600

    js added

--- script/script.js
+++ script/script.js
@@ -14,3 +14,12 @@ const showsidebar=(togglId,sidebarId,headerId,mainId)=>{
 }
 
 showsidebar('header_toggle','sidebar','header','main');
+
+
+const sidebarLink=document.querySelectorAll('sidebar__list a');
+
+function linkColor(){
+    sidebarLink.forEach(l=>l.classList.remove('active-link'));
+    this.classList.add('active-link');
+}
+sidebarLink.forEach(l=>l.addEventListener('click',linkColor));
